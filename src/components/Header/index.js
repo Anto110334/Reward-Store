bf916e2a8da5b007d4d52a1faa186ca15283716d
Anto110334 <anto.merlo.ruggieri@gmail.com>
2021-05-31 22:52:40 -0300

    [FIXED] - route path

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -15,8 +15,8 @@ const Header = props => {
   const [show, setShow] = useState(false);
   const [keyActive, setKeyActive] = useState(1);
   const navItems = [
-    {id: 1, name: 'Inicio', url: '/'},
-    {id: 2, name: 'Historial', url: '/historial'},
+    {id: 1, name: 'Inicio', url: '/Reward-Store'},
+    {id: 2, name: 'Historial', url: '/Reward-Store/historial'},
   ];
 
   const handleClickActivate = (navItem) => {
